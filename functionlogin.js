80c5c9df30fdfa1b0ca46fe341f2421310b2408a
fridaasmara <113350657+fridaasmara@users.noreply.github.com>
2023-10-04 15:01:21 +0700

    Add files via upload

---
+++ functionlogin.js
@@ -0,0 +1,10 @@
+function login (username, pw) {
+    console.log("------------------------")
+    console.log("          Login         ")
+    console.log("------------------------")
+    console.log("Username : " + username)
+    console.log("Password : " + pw)
+    console.log("------------------------")
+}
+
+login("fridaasmara", "fffff")
---
+++ functionlogin1.js
@@ -0,0 +1,10 @@
+const login = (username, pw) => {
+    console.log("------------------------")
+    console.log("          Login         ")
+    console.log("------------------------")
+    console.log("Username : " + username)
+    console.log("Password : " + pw)
+    console.log("------------------------")
+}
+
+login("fridaasmara", "123")
